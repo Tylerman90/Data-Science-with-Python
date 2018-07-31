2258f4c818b5359be66b769910de99c254c65b45
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-07-30 21:52:55 -0400

    learning basic numpy methods

--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 import numpy as np
 my_list = [1,2,3]
 my_mat = [[1,2,3],[4,5,6],[7,8,9]]
-print(np.array(my_mat))
-print(np.arange(0,10))
+np.array(my_mat)
+np.arange(0,11,2)
+np.zeros((2,3))
+np.ones((3,4))
+np.linspace(0,5,10)
+np.eye(4)
+np.random.randint(1,100,10)
+
+arr = np.arange(25)
+ranarr = np.random.randint(0,50,10)
+print(ranarr)
+
+arr.reshape(5,5)
+print(ranarr.max())
+print(ranarr.min())
+print(ranarr.argmin())
+
+
+
