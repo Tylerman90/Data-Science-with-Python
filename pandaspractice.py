4c725d4e2622abdb3266aedc6f0e65315a02e0f3
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-08-04 15:37:34 -0400

    creating new columns for dataframes

--- pandaspractice.py
+++ pandaspractice.py
@@ -5,3 +5,17 @@ labels = ['a','b','c']
 my_data = [10,20,30]
 arr = np.array(my_data)
 d  = {'a':10,'b':20,'c':30}
+
+#These all do the same thing when converted into a series
+print(pd.Series(data = my_data))
+print(pd.Series(my_data,labels))
+print(pd.Series(arr))
+print(pd.Series(d))
+
+ser1 = pd.Series([1,2,3,4],['USA','Germany','USSR','Japan'])
+print(ser1)
+
+ser2 = pd.Series([1,2,5,4],['USA','Germany','Italy','Japan'])
+print(ser2)
+
+print(ser1 + ser2)
