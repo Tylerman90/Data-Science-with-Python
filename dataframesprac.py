8e6a624cc1d382e96e5e8063877ce19c99b86e5a
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-08-05 12:07:58 -0400

    conditional selection

--- dataframesprac.py
+++ dataframesprac.py
@@ -7,3 +7,20 @@ np.random.seed(101)
 df = pd.DataFrame(randn(5,4),['A','B','C','D','E'],['W','X','Y','Z'])
 print(df)
 print(df[['W','Z']])
+
+#Creating a new columns
+df['new'] = df['W'] + df['Y']
+print(df)
+
+#to remove a column
+df.drop('new',axis=1,inplace=True)
+print(df)
+
+#to remove a row
+print(df.drop('E'))
+
+#conditional selection that brings back boolean values
+booldf = df > 0
+print(booldf)
+#conditional selection with bracket notation to bring back numbers
+print(df[booldf])
