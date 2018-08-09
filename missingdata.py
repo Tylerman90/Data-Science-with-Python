a3584d0e43f3c4677448124b6a9f1c727f3c54b4
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-08-09 18:30:14 -0400

    drop null data in rows and columns

--- missingdata.py
+++ missingdata.py
@@ -3,3 +3,11 @@ import pandas as pd
 
 
 d = {'A':[1,2,np.nan],'B':[5,np.nan,np.nan],'C':[1,2,3]}
+df = pd.DataFrame(d)
+print(df)
+
+#to drop null vaulues along rows. default axis=0
+print(df.dropna())
+
+#to drop null values along columns. argument axis=1
+print(df.dropna(axis=1))
